f426cc0427cb4e04672ad171c702c1b3bf3b4d9b
Guilherme Gianjope <guigianjope@gmail.com>
2021-06-02 17:24:21 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,81 +1,111 @@
-import shoes1 from './shoes1.jpg';
-import shoes2 from './shoes2.jpg';
-import shoes3 from './shoes3.jpg';
-import shoes4 from './shoes4.jpg';
+import React from 'react';
 import './App.css';
+import Colors from './components/Colors'
+import DetailsThumb from './components/DetailsThumb';
 
-function App() {
-  return (
-    <div className="card-wrapper">
-      <div className="card">
-        {/* left card */}
-        <div className="product-imgs">
-          <div className="img-display">
-            <div className="img-showcase">
-              <img src={shoes1} alt="shoes"/>
-              <img src={shoes2} alt="shoes"/>
-              <img src={shoes3} alt="shoes"/>
-              <img src={shoes4} alt="shoes"/>
+import shoes1 from './img/shoes1.jpg';
+import shoes2 from './img/shoes2.jpg';
+import shoes3 from './img/shoes3.jpg';
+import shoes4 from './img/shoes4.jpg';
+
+class App extends React.Component{
+
+  state = {
+    products: [
+      {
+        "_id": "1",
+        "title": "Shoes",
+        "brand": "Nike",
+        "src": [
+            shoes1,
+            shoes2,
+            shoes3,
+            shoes4
+          ],
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "category": "Shoes",
+
+        "price": 180,
+        "colors":["blue","black","orange","teal"],
+        "count": 1
+      }
+    ],
+    index: 0
+  };
+
+  myRef = React.createRef();
+
+  handleTab = index =>{
+    this.setState({index: index})
+    const images = this.myRef.current.children;
+    for(let i=0; i<images.length; i++){
+      images[i].className = images[i].className.replace("active", "");
+    }
+    images[index].className = "active";
+  };
+
+  componentDidMount(){
+    const {index} = this.state;
+    this.myRef.current.children[index].className = "active";
+  }
+
+
+  render(){
+    const {products, index} = this.state;
+    return(
+      <div className="app">
+        {
+          products.map(item =>(
+            <div className="card-wrapper" key={item._id}>
+
+                <div className="product-imgs">
+
+                  <div className="img-showcase">
+                    <img src={item.src[index]} alt=""/>
+                  </div>
+
+                  <div className="img-select">
+                    <DetailsThumb images={item.src} tab={this.handleTab} myRef={this.myRef} />
+                  </div>  
+                  
+                </div>
+
+                <div className="product-content">
+                                        
+                    <h2 className="product-title">{item.title}</h2>
+
+                    <div className="product-price">
+                      <p className="price">Price: <span>${item.price}</span></p>
+                    </div>
+                    
+                    <div className="product-detail">
+                      <h2>Description:</h2>
+                      <p>{item.description}</p>
+                      <Colors colors={item.colors} />
+                      <ul>
+                        <li>Brand: <span>{item.brand}</span></li>
+                        <li>Availability: <span>in stock</span></li>
+                        <li>Category: <span>{item.category}</span></li>
+                        <li>Shipping Area: <span>All North America</span></li>
+                        <li>Shipping Fee: <span>Free</span></li>
+                      </ul>
+                    </div>
+
+                    
+                    <div className="purchase-info">
+                      <input type="number" min="0" value="1"></input>
+                      <button type="button" className="btn">Add to Cart</button>
+                      <button type="button" className="btn">Buy</button>
+                    </div>
+
+                </div>
+              
             </div>
-          </div>
-
-          <div className="img-select">
-            <div className="img-item">
-              <a href="#" id="1">
-                <img src={shoes1} alt="shoes main"/>
-              </a>
-            </div>
-            <div className="img-item">
-              <a href="#" id="2">
-                <img src={shoes2} alt="shoes2 details"/>
-              </a>
-            </div>
-            <div className="img-item">
-              <a href="#" id="3">
-                <img src={shoes3} alt="shoes3 details"/>
-              </a>
-            </div>
-            <div className="img-item">
-              <a href="#" id="4">
-                <img src={shoes4} alt="shoes4 details"/>
-              </a>
-            </div>
-          </div>
-        </div>
-
-        {/* right card */}
-        <div className="product-content">
-          <h2 className="product-title">Shoes</h2>
-          
-          <div className="product-price">
-            <p className="price">Price: <span>$180.00</span></p>
-          </div>
-
-          <div className="product-detail">
-            <h2>Description:</h2>
-            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-            <ul>
-            <li>Brand: <span>Nike</span></li>
-            <li>Color: <span>White</span></li>
-            <li>Availability: <span>in stock</span></li>
-            <li>Category: <span>Shoes</span></li>
-            <li>Shipping Area: <span>All North America</span></li>
-            <li>Shipping Fee: <span>Free</span></li>
-          </ul>
-          </div>
-
-          <div className="purchase-info">
-            <input type="number" min="0" value="1"></input>
-            <button type="button" className="btn">Add to Cart</button>
-            <button type="button" className="btn">Buy</button>
-          </div>
-
-        </div>
-
-        
-      </div>      
-    </div>
-  );
+          ))
+        }
+      </div>
+    );
+  };
 }
 
 export default App;
